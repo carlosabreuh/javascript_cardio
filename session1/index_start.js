c050ee9fd633b82c1a5f0fe6ef6a665c309a53fd
carlosabreuh <carlos@abreuh.com>
2021-03-19 13:22:45 -0400

    Update index_start.js

--- session1/index_start.js
+++ session1/index_start.js
@@ -1,9 +1,41 @@
 // CHALLENGE 1: REVERSE A STRING
 // Return a string in reverse
-// ex. reverseString('hello') === 'olleh'
+// ex. reverseString('carlos') === 'solrac'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  // return str
+  //   .split('');
+  // .reverse();
+  // .join('');
+
+  ///////////////////////////////////////////
+
+  // let revString = '';
+  // for(let i = str.length - 1; i >= 0; i--) {
+  //   revString = revString + str[i];
+    
+  // }
+  // return revString;
+// let revString = '';
+// for (let i = 0; i <= str.length - 1; i++) {
+//   revString = str[i] + revString;
+//   }
+//   return revString;
+
+  ///////////////////////////////////////////
+
+// let revString = '';
+// for(let char of str) {
+//   revString = char + revString;
+// }
+// return revString;
+
+
+let revString = '';
+for(let char of str) {
+  revString = char + revString;
+}
+return revString;
 }
 
 
